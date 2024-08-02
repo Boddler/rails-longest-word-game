6273b4d95a3d11b56f0cf1e81b13e44095dccc52
Barry Roberts <barryjroberts@gmail.com>
2024-08-02 21:27:27 +0900

    fixed dictionary call

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,13 @@
-class GamesController < ApplicationController
+require "open-uri"
 
+class GamesController < ApplicationController
   def new
     @letters = []
-    10.times do
-      @letters << rand(65..90).chr
+    7.times do
+      @letters << %w[b c d f g h j k l m n p q r s t v w x y z].sample.upcase
+    end
+    3.times do
+      @letters << %w[a e i o u].sample.upcase
     end
   end
 
@@ -11,20 +15,20 @@ class GamesController < ApplicationController
     x = 0
     @letters = params[:letters].split("")
     @answer = params[:answer].upcase.split("")
-    @lettershash = @letters.inject(Hash.new(0)) { |total, letter| total[letter] += 1;total }
-    @answerhash = @answer.inject(Hash.new(0)) { |total, letter| total[letter] += 1;total }
+    @lettershash = @letters.inject(Hash.new(0)) { |total, letter| total[letter] += 1; total }
+    @answerhash = @answer.inject(Hash.new(0)) { |total, letter| total[letter] += 1; total }
     @answerhash.each do |key, value|
       x += 1 if value > @lettershash[key]
     end
-    api = {}
-    api = HTTParty.get('https://wagon-dictionary.herokuapp.com/#{params[:answer]}')
+    api = URI.open("https://dictionary.lewagon.com/#{params[:answer]}")
+    json = JSON.parse(api.read)
     if x > 0
-    @result = "Sorry but your word can't be made from those letters."
-    elsif condition
-      raise
+      @result = "Sorry but your word can't be made from those letters."
+    elsif x.zero? && json["error"]
+      @result = "Sorry, but your word isn't a word."
     else
-      @result = "That's a valid word - great job!"
+      score = json["length"]
+      @result = "That's a valid word - great job! #{score} points!"
     end
   end
-
 end
